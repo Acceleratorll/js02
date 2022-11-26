0f739a60f71d7f85b5c9083e888a366097ad71f4
Moch Fajrul Falah <mfajrulfalah.ff@gmail.com>
2022-11-26 19:03:18 +0700

    2. Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      home: Image.asset('assets/Owl.jpg')
+    ); 
   }
 }
 
